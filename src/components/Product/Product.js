672575951fb6cf57e976753e6c2bc1a6d98a0520
shakil-ahmmed-se <mdshakilahmmed8906@gmail.com>
2024-08-31 08:12:07 -0700

    ema john simple website with react

---
+++ src/components/Product/Product.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import './Product.css';
+const Product = (props) => {
+    const { img, name, seller, price, stock } = props.product;
+    return (
+        <div className='product'>
+            <div>
+                <img src={img} alt="product" />
+            </div>
+            <div className='product_description'>
+                <h3 className='product_name'>{name}</h3>
+
+                <p><small>By:{seller}</small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in Stock- Order Soon</small></p>
+                <button onClick={()=>props.handleAddProduct(props.product)} className='add_cart_btn'>
+                <FontAwesomeIcon icon={faShoppingCart} /> Add to cart
+                </button>
+            </div>
+
+        </div>
+    );
+};
+
+
+
+export default Product;
---
+++ src/components/Shop/shop.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import fakeData from '../../fakeData'
+import './shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
+const Shop = () => {
+    const first10 = fakeData.slice(0, 10);
+    const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([])
+    const handleAddProduct = (product) =>{
+        const newCart = [...cart, product];
+        setCart(newCart)
+    }
+    return (
+        <div className='shop_container'>
+            <div className="product_container">
+
+                {
+                    products.map(product => <Product product={product} key={product.key} handleAddProduct={handleAddProduct}></Product>)
+                }
+
+            </div>
+            <div className="cart_container">
+                <Cart cart={cart}></Cart>
+            </div>
+
+        </div>
+    );
+};
+
+export default Shop;
